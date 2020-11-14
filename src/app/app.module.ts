44b8e2ecbb9449ea1a46af1d3b3ce0dcfb067ade
abhishek <abhishek4murumkar@gmail.com>
2020-11-14 13:57:06 +0530

    Added Routing in Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,18 @@ import {MatButtonModule} from '@angular/material/button';
 
 import 'hammerjs';
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { DishDetailComponent } from './dish-detail/dish-detail.component';
 import { DishService } from "./services/dish.service";
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
   declarations: [
@@ -23,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +42,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [
     DishService
